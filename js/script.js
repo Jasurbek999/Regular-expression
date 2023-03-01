265844a7fbd0a6c001b1cef8252db18d3f6c90b2
Jasurbek999 <118923128+Jasurbek999@users.noreply.github.com>
2023-03-01 07:52:44 +0500

    regax

---
+++ js/script.js
@@ -0,0 +1,40 @@
+ 'use srict'
+
+// const firstname= prompt('What is your name?', '')
+
+// const regex=/a/g
+
+// //console.log(firstname.search(regex))
+// // console.log(firstname.match(regex))
+
+// const password = prompt('Your password')
+
+// console.log(password.replace(/\$/g, '*'))
+
+// const num="23-33-34-132-323"
+
+// console.log(num.replace(/-/g, ':' ))
+
+const name=prompt('Name', '')
+
+const regax = /\s/g
+
+console.log(name.match(regax))
+
+
+
+// //console.log(regax.test(name))
+
+// if(regax.match(name)){
+//   console.log('Welcome samar')
+// }else{
+//   console.log('You are not admin')
+// }
+
+// \d - numberlarni qidiradi
+// \w - worl so`zlarni qidiradi
+// \s - space space larni qidiradi
+
+// \D - not a number
+// \W - not a word
+// \S - not a spice 
